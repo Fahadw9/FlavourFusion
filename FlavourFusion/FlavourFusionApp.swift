534bfc99288c69940d3c6b250ac77b05644bf5c9
Fahad Waseem <fahadwaseem8@gmail.com>
2024-05-24 21:14:44 +0500

    Initial Commit

---
+++ FlavourFusion/FlavourFusionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavourFusionApp.swift
+//  FlavourFusion
+//
+//  Created by Fahad Waseem on 24/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlavourFusionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
